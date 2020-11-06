3cba49181c98a604e50eadf9270c5c3e5de50543
Ananth Kalki <ananthkalki@gmail.com>
2020-11-06 17:49:52 +0530

    partial view implemented for line items (same model and controller)

---
+++ SalesApp/SalesApp/SalesApp/Models/VPDModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalesApp.Models
+{
+    public class VPDModel
+    {
+        [Display(Name = "Order#")]
+        [StringLength(10)]
+        public string vpd1 { get; set; }
+
+        [Display(Name = "Vendor")]
+        [StringLength(35)]
+        public string PS02013 { get; set; }
+
+        [Display(Name = "Currency")]
+        [StringLength(35)]
+        public string vpd2 { get; set; }
+
+        public List<SelectListItem> Vendor = new List<SelectListItem>();
+        public List<SelectListItem> Currency = new List<SelectListItem>();
+
+    }
+}
