7e6bb9e916fd5f18a0f145d34f9bc738ec0123f8
Ananth Kalki <ananthkalki@gmail.com>
2020-11-10 17:18:58 +0530

    FRT front end done

---
+++ SalesApp/SalesApp/SalesApp/Controllers/FRTController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SalesApp.Models;
+
+namespace SalesApp.Controllers
+{
+    public class FRTController : Controller
+    {
+        // GET: FRT
+        public ActionResult Index()
+        {
+            FRTModel RM = new FRTModel();
+            List<SelectListItem> delmeth = new List<SelectListItem>();
+            delmeth.Add(new SelectListItem
+            {
+                Text = "Arjun",
+                Value = "164"
+            });
+            delmeth.Add(new SelectListItem
+            {
+                Text = "Sreekanth",
+                Value = "370"
+            });
+            delmeth.Add(new SelectListItem
+            {
+                Text = "Edwin",
+                Value = "187"
+            });
+            RM.FRT3 = "370";
+            RM.Delivery_Method = delmeth;
+
+            List<SelectListItem> currency = new List<SelectListItem>();
+            currency.Add(new SelectListItem
+            {
+                Text = "Arjun",
+                Value = "164"
+            });
+            currency.Add(new SelectListItem
+            {
+                Text = "Sreekanth",
+                Value = "370"
+            });
+            currency.Add(new SelectListItem
+            {
+                Text = "Edwin",
+                Value = "187"
+            });
+            RM.FRT4= "370";
+            RM.Currency = currency;
+
+            List<SelectListItem> vendor = new List<SelectListItem>();
+            vendor.Add(new SelectListItem
+            {
+                Text = "Arjun",
+                Value = "164"
+            });
+            vendor.Add(new SelectListItem
+            {
+                Text = "Sreekanth",
+                Value = "370"
+            });
+            vendor.Add(new SelectListItem
+            {
+                Text = "Edwin",
+                Value = "187"
+            });
+            RM.FRT5 = "370";
+            RM.Vendor = vendor;
+
+            List<SelectListItem> source = new List<SelectListItem>();
+            source.Add(new SelectListItem
+            {
+                Text = "Arjun",
+                Value = "164"
+            });
+            source.Add(new SelectListItem
+            {
+                Text = "Sreekanth",
+                Value = "370"
+            });
+            source.Add(new SelectListItem
+            {
+                Text = "Edwin",
+                Value = "187"
+            });
+            RM.FRT6 = "370";
+            RM.Source = source;
+
+            List<SelectListItem> destination = new List<SelectListItem>();
+            destination.Add(new SelectListItem
+            {
+                Text = "Arjun",
+                Value = "164"
+            });
+            destination.Add(new SelectListItem
+            {
+                Text = "Sreekanth",
+                Value = "370"
+            });
+            destination.Add(new SelectListItem
+            {
+                Text = "Edwin",
+                Value = "187"
+            });
+            RM.FRT7 = "370";
+            RM.Destination = destination;
+
+            return View(RM);
+        }
+    }
+}
---
+++ SalesApp/SalesApp/SalesApp/Models/FRTModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalesApp.Models
+{
+    public class FRTModel
+    {
+        [Display(Name = "RFQ Number")]
+        [StringLength(10)]
+        public string FRT1 { get; set; }
+        
+        [Display(Name = "Customer")]
+        [StringLength(10)]
+        public string FRT2 { get; set; }
+
+        [Required(ErrorMessage = "Please select Delivery Method.")]
+        [Display(Name = "Delivery Method")]
+        public string FRT3 { get; set; }
+
+        [Required(ErrorMessage = "Please select Currency.")]
+        [Display(Name = "Currency")]
+        public string FRT4 { get; set; }
+
+        [Required(ErrorMessage = "Please select Vendor.")]
+        [Display(Name = "Vendor")]
+        public string FRT5 { get; set; }
+
+        [Required(ErrorMessage = "Please select Source.")]
+        [Display(Name = "Source")]
+        public string FRT6 { get; set; }
+
+        [Required(ErrorMessage = "Please select Destination.")]
+        [Display(Name = "Destination")]
+        public string FRT7 { get; set; }
+
+        [Display(Name = "Weight(KG)")]
+        public int FRT8 { get; set; }
+
+        [Display(Name = "Rate(USD)")]
+        public int FRT9 { get; set; }
+
+        [Display(Name = "Frieght Budget Variation (USD)")]
+        [StringLength(10)]
+        public string FRT10 { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Justification")]
+        public string FRT11 { get; set; }
+
+        public List<SelectListItem> Delivery_Method = new List<SelectListItem>();
+        public List<SelectListItem> Currency = new List<SelectListItem>();
+        public List<SelectListItem> Vendor = new List<SelectListItem>();
+        public List<SelectListItem> Source = new List<SelectListItem>();
+        public List<SelectListItem> Destination = new List<SelectListItem>();
+
+    }
+}
